073e92b72a4cec5799ddd47eace7f3ddaf486115
Ramya <ramya@Ramya>
2020-06-02 19:59:31 +0530

    Sample test program

---
+++ calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Calculator program containing basic arithmetic operations
+"""
+
+def add_two_numbers(first_num, second_num):
+    return first_num + second_num
+
+def substract_two_numbers(first_num, second_num):
+    return first_num - second_num
+
