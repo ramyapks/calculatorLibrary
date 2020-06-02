fcef1712cb4b6a1deee708d022364043ef9508e4
Ramya <ramya@Ramya>
2020-06-02 22:35:35 +0530

    changed for checking CI

--- calculator.py
+++ calculator.py
@@ -4,9 +4,10 @@
 Calculator program containing basic arithmetic operations
 """
 
-def add_two_numbers(first_num, second_num):
-    return first_num + second_num
+
+def add_two_numbers(first_num, sec_num):
+    return first_num + sec_num
+
 
 def substract_two_numbers(first_num, second_num):
     return first_num - second_num
-
