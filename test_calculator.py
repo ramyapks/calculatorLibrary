4c54dacbfa25c6923a4ae7ebb8445238eb57fa92
Ramya <ramya@Ramya>
2020-06-02 23:28:11 +0530

    Removed assertions

--- test_calculator.py
+++ test_calculator.py
@@ -9,8 +9,10 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add_two_numbers(2,2)
+        if(4 != calculator.add_two_numbers(2,2)):
+            raise Exception('Failed addition')
 
     def test_sub(self):
-        assert 2 == calculator.substract_two_numbers(4,2)
+        if(2 != calculator.substract_two_numbers(4,2)):
+            raise Exception('Failed substraction')
 
